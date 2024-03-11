e50bf9db9804a4f719dce847ee9375c3a9eca354
cwt_cherry <wing.chan-6@student.manchester.ac.uk>
2024-03-11 17:57:31 +0000

    default, parameterised, copy constructor, destructor, test vector

--- assignment-4.cpp
+++ assignment-4.cpp
@@ -1,8 +1,6 @@
 // PHYS 30762 Programming in C++
 // Assignment 4
 // Practice special functions and operators in C++ classes
-// Note that the hints in the skeleton are given to help you
-// in case of doubt, the official guidance/marking scheme is on the slides on BB 
 
 #include<iostream>
 #include<string>
@@ -15,20 +13,53 @@ using std::string;
 class particle
 {
 private:
-  string particle_name;
-  //...other data members (see slides on BB)
+  string name;
+  int charge{1}; // -1 or +1
+  const double speed_of_light = 2.9979245e8; // Unit of m/s
+  bool antiparticle;
+  double energy{0}; // Between 0 and c
+  double momentum_x{0};
+  double momentum_y{0};
+  double momentum_z{0};
+  std::vector<double> four_momentum;
+  std::vector<double>* four_momentum_copy;
   // We need the four-vector, you can leave the particle mass (or remove it, we don't mark it)
 
 
 public:
-  // Constructors
-  // Here you need a default constructor, a parameterised constructor and a copy constructor
+  // Default Constructor
+  particle(): name{}, charge{}, antiparticle{}, energy{}, momentum_x{}, momentum_y{}, momentum_z{}
+  {
+  }
+
+  // Parameterised Constructor
   // The parameterised constructor needs to dynamically allocate the std::vector containing the four-vector elements
   // The parameterised constructor also needs to check the validity of the energy component
-  // The copy constructor needs to make a deep copy of the std::vector holding the 4-momentum
+  particle(string particle_name, int particle_charge, bool particle_antiparticle, double particle_energy, double particle_momentum_x, double particle_momentum_y, double particle_momentum_z):
+    name{particle_name}, charge{particle_charge}, antiparticle(particle_antiparticle), energy{particle_energy}, momentum_x{particle_momentum_x}, momentum_y{particle_momentum_y}, momentum_z{particle_momentum_z}
+  {
+    four_momentum.push_back(energy);
+    four_momentum.push_back(momentum_x);
+    four_momentum.push_back(momentum_y);
+    four_momentum.push_back(momentum_z);
+  }
+
+  // Copy constructor
+  particle(const particle& copy):
+    name{copy.name}, charge{copy.charge}, antiparticle{copy.antiparticle}, energy{copy.energy}, momentum_x{copy.momentum_x}, momentum_y{copy.momentum_y}, momentum_z{copy.momentum_z}
+  {
+    // The copy constructor needs to make a deep copy of the std::vector holding the 4-momentum
+    // Copy constructor with dynamic allocation
+    four_momentum_copy = new std::vector<double>(copy.four_momentum);
+  }
 
   // Destructor
-  // The destructor needs to free the memory allocated by the constructor
+  ~particle()
+  {
+    std::cout<<"Destroying "<<name<<std::endl;
+    // The destructor needs to free the memory allocated by the constructor
+    delete four_momentum_copy;
+  }
 
   // Assignment operator
   // The assignment operator needs to avoid self-assignment using the *this pointer
@@ -40,10 +71,52 @@ public:
   // The move assignment operator needs to correctly reassign the memory from the original object
   
   // Getter functions (accessors) to individual elements of 4-momentum
-  // This should include function returning beta value 
+  double get_energy() const
+  {
+    return energy;
+  }
+
+  double get_momentum_x() const
+  {
+    return momentum_x;
+  }
+
+  double get_momentum_y() const
+  {
+    return momentum_y;
+  }
+
+  double get_momentum_z() const
+  {
+    return momentum_z;
+  }
 
   // Setter functions, to change values of 4-momentum 
+  void set_momentum_x(double particle_energy)
+  {
+    momentum_x = particle_energy / speed_of_light;
+  }
+  void set_momentum_y(double particle_energy)
+  {
+    momentum_y = particle_energy / speed_of_light;
+  }
+  void set_momentum_z(double particle_energy)
+  {
+    momentum_z = particle_energy / speed_of_light;
+  }
   // Make sure you check input validity for the energy in the 4-momentum 
+  void set_energy(double particle_energy)
+  {
+    if(speed_of_light >= particle_energy && particle_energy >= 0)
+    {
+      energy = particle_energy;
+    }
+    else
+    {
+      std::cerr<<"Invalid energy. Energy should be between 0 and the speed of light."<<std::endl;
+      exit(0);
+    }
+  }
 
   // Function to print info about a particle 
   // (not necessary or marked, but nice as you can extend the one you already have from Assignment 3)
@@ -55,13 +128,42 @@ public:
 
 // End of particle class and associated member functions
 
+// Define variable
+const double speed_of_light = 2.9979245e8; // Unit of m/s
+
+// Random velocity generating function
+double random_energy() 
+{
+  return static_cast<double>(std::rand()) / RAND_MAX * speed_of_light;
+}
+
 // Main program
 int main()
 {
+  particle p;
 
   // Create the following particles: 
   // two electrons, four muons, one antielectron, one antimuon 
   // Use the parameterised constructor to do these
+  particle electron1("electron", -1, false, random_energy(), p.get_momentum_x(), p.get_momentum_y(), p.get_momentum_z());
+  particle electron2("electron", -1, false, random_energy(), p.get_momentum_x(), p.get_momentum_y(), p.get_momentum_z());
+  particle electron3("positron", 1, true, random_energy(), p.get_momentum_x(), p.get_momentum_y(), p.get_momentum_z());
+  particle muon1("muon", -1, false, random_energy(), p.get_momentum_x(), p.get_momentum_y(), p.get_momentum_z());
+  particle muon2("muon", -1, false, random_energy(), p.get_momentum_x(), p.get_momentum_y(), p.get_momentum_z());
+  particle muon3("muon", -1, false, random_energy(), p.get_momentum_x(), p.get_momentum_y(), p.get_momentum_z());
+  particle muon4("muon", -1, false, random_energy(), p.get_momentum_x(), p.get_momentum_y(), p.get_momentum_z());
+  particle muon5("antimuon", 1, true, random_energy(), p.get_momentum_x(), p.get_momentum_y(), p.get_momentum_z());
+
+  std::vector<particle> particles;
+
+  particles.push_back(electron1);
+  particles.push_back(electron2);
+  particles.push_back(electron3);
+  particles.push_back(muon1);
+  particles.push_back(muon2);
+  particles.push_back(muon3);
+  particles.push_back(muon4);
+  particles.push_back(muon5);
 
   // (optional but nice) Print out the data from all the particles (put them in a vector)
 
